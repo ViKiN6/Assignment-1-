f35a840da16692fcc0fbfbdad21e837e301b3ca4
ViKiN6 <5100164@mypillar7uwc.ac.za>
2023-12-07 14:04:29 +0200

    Assignment 1

---
+++ Milksong.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Milksong
+    {
+        public static string Milk()
+        {
+            for (int i = 99; i > 0; i--)
+
+            {
+                if (i >= 3)
+                {
+                    Console.WriteLine($"{i} bottles of milk on the wall, {i} bottles of milk.");
+                    Console.WriteLine($"Take one down and pass it around, {i - 1} bottles of milk");
+                }
+
+                if (i == 2)
+                {
+                    Console.WriteLine($"{i} bottles of milk on the wall, {i} bottles of milk.");
+
+                    Console.WriteLine($"Take one down and pass it around, {i - 1} bottle of milk");
+                }
+                if (i == 1)
+                {
+                    Console.WriteLine($"{i} bottle of milk on the wall, {i} bottle of milk.");
+                    break;
+                }
+            }            
+            return "";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Milksong");
+
+            string milksong = Milksong.Milk();
+            
+        }
+    }
+}
